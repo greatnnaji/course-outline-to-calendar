14b83dde7a27c533d4cf84b05be71df6513b6cf8
great nnaji <nnajiuchegreat2004@gmail.com>
2026-05-30 19:24:35 -0400

    chore: update backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://course-outline-to-calendar.vercel.app"],    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
